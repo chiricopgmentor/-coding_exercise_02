ebf33b0ef6688452326e6112804e89d3aee23212
chiricopgmentor <chiricoprogmentor@gmail.com>
2021-06-23 11:13:38 +0900

    スティッキーメニュー実装

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+// カルーセル
 $('.carousel').slick({
   // autoplay: true,
   arrows: false,
+});
+
+$window = $(window);
+
+// スティッキーメニュー
+$window.scroll(function() {
+  const window_scroll = $(this).scrollTop();
+  const window_height = $window.height();
+  const $head_menu = $('.l-header .section_container');
+  const is_sticky = 'section_container--sticky_menu';
+
+  if (window_scroll > window_height) {
+    $head_menu.addClass(is_sticky);
+  } else {
+    $head_menu.removeClass(is_sticky);
+  }
 });
