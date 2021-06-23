77dbe4b5b6cd956e2bf1e04d13e8893073ae7582
chiricopgmentor <chiricoprogmentor@gmail.com>
2021-06-23 13:15:28 +0900

    ヘッダーメニューの構成修正

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $window = $(window);
 $window.scroll(function() {
   const window_scroll = $(this).scrollTop();
   const window_height = $window.height();
-  const $head_menu = $('.l-header .section_container');
+  const $head_menu = $('.ly_headerContainer');
   const is_sticky = 'section_container--sticky_menu';
 
   if (window_scroll > window_height) {
