fd623bce359538b5237b09e4d204047c833d6092
chiricopgmentor <chiricoprogmentor@gmail.com>
2021-06-29 17:53:15 +0900

    レスポンシブ対応（concept）

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+const breakpoint = 768;
+
 // カルーセル
-$('.bl_bgiCarousel').slick({
+$(".bl_bgiCarousel").slick({
   autoplay: true,
   arrows: false,
 });
@@ -7,15 +9,18 @@ $('.bl_bgiCarousel').slick({
 $window = $(window);
 
 // スティッキーメニュー
-$window.scroll(function() {
-  const window_scroll = $(this).scrollTop();
-  const window_height = $window.height();
-  const $head_menu = $('.ly_header_inner');
-  const is_sticky = 'ly_header_inner__sticky';
+$window.scroll(function () {
+  const window_width = $window.width();
+  if (window_width >= breakpoint) {
+    const window_scroll = $(this).scrollTop();
+    const window_height = $window.height();
+    const $head_menu = $(".ly_header_inner");
+    const is_sticky = "ly_header_inner__sticky";
 
-  if (window_scroll > window_height) {
-    $head_menu.addClass(is_sticky);
-  } else {
-    $head_menu.removeClass(is_sticky);
+    if (window_scroll > window_height) {
+      $head_menu.addClass(is_sticky);
+    } else {
+      $head_menu.removeClass(is_sticky);
+    }
   }
 });
