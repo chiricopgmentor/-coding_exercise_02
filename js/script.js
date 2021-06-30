34f06ad2fa81307d26e84eaf7ad370932312da8d
chiricopgmentor <chiricoprogmentor@gmail.com>
2021-06-30 11:45:56 +0900

    画面リサイズイベントを追加

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 const breakpoint = 768;
+$window = $(window);
+const $header = $(".ly_header");
+const is_sticky = "ly_header__sticky";
 
-// カルーセル
+// 背景カルーセル
 $(".bl_bgiCarousel").slick({
   autoplay: true,
   arrows: false,
 });
 
-$window = $(window);
-
-// スティッキーメニュー
+// スクロールイベント
 $window.scroll(function () {
   const window_width = $window.width();
   if (window_width >= breakpoint) {
     const window_scroll = $(this).scrollTop();
     const window_height = $window.height();
-    const $header = $(".ly_header");
-    const is_sticky = "ly_header__sticky";
-
     if (window_scroll > window_height) {
+      // スティッキーメニュー
       $header.addClass(is_sticky);
     } else {
       $header.removeClass(is_sticky);
     }
+  }
+});
+
+// 画面リサイズイベント
+$window.resize(function () {
+  const window_width = $window.width();
+  if (window_width >= breakpoint) {
+    ;
+  } else {
+    $header.removeClass(is_sticky);
   }
 });
