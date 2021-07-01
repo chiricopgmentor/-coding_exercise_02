a88c2525f473ff5e52687e78bf258cd3ba32a6fb
chiricopgmentor <chiricoprogmentor@gmail.com>
2021-07-02 07:03:55 +0900

    js関連のクラスをPRECSSに従って整理

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 const breakpoint = 768;
 $window = $(window);
-const $body = $('body')
-const $header = $('.ly_header');
-const $headerNav = $('.bl_headerNav');
-const $humburgerBtn = $('.bl_humburgerBtn');
+const $body = $('.js_body')
+const $header = $('.js_header');
+const $headerNav = $('.js_headerNav');
+const $humburgerBtn = $('.js_humburgerBtn');
 const $bgiCarousel = $('.js_bgiCarousel');
-const is_sticky = 'ly_header__sticky';
-const is_btn_cross = 'bl_humburgerBtn__cross';
-const is_menuClose = 'bl_headerNav__close';
-const is_menuOpen = 'bl_headerNav__spMenuOpen';
+const is_sticky = 'is_sticky';
+const is_cross = 'is_cross';
+const is_close = 'is_close';
+const is_open = 'is_spMenuOpen';
 const is_noscroll = 'is_noScroll';
 
 // 画面サイズに応じたクラス付け替え
 function responsiveClassJustify() {
   const window_width = $window.width();
   if (window_width >= breakpoint) {
-    $headerNav.removeClass(is_menuClose);
+    $headerNav.removeClass(is_close);
     $body.removeClass(is_noscroll);
   } else {
     $header.removeClass(is_sticky);
-    $headerNav.addClass(is_menuClose);
+    $headerNav.addClass(is_close);
   }
 }
 // 初期表示時と画面リサイズ時に実行する
@@ -49,9 +49,9 @@ $window.scroll(function () {
 
 // ハンバーガーボタンクリックイベント
 $humburgerBtn.click(function () {
-  $humburgerBtn.toggleClass(is_btn_cross);
-  $headerNav.toggleClass(is_menuClose);
-  $headerNav.toggleClass(is_menuOpen);
+  $humburgerBtn.toggleClass(is_cross);
+  $headerNav.toggleClass(is_close);
+  $headerNav.toggleClass(is_open);
   $body.toggleClass(is_noscroll);
 });
 
